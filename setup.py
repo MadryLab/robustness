504bacff49763ec8dec45cfcbb130dea7419710c
Andrew Ilyas <ailyas@mit.edu>
2020-07-10 00:34:13 -0400

    increment version in setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
   # Versions should comply with PEP440.  For a discussion on single-sourcing
   # the version across setup.py and the project code, see
   # https://packaging.python.org/en/latest/single_source_version.html
-  version='1.2',
+  version='1.2.post1',
 
   description='Tools for Robustness',
   long_description=long_description,
